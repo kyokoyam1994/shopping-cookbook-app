4750ede547aa2c44d9b08673c154a99a03be3af0
Kosei Yokoyama <kyokoyama@ridgelineintl.com>
2020-08-21 00:01:17 -0400

    Finished modules and optimization section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RouterModule, Routes} from '@angular/router';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipesEditComponent } from './recipes/recipes-edit/recipes-edit.component';
-import { RecipesResolverService } from './recipes/recipes-resolver.service';
-import { AuthComponent } from './auth/auth.component';
-import { AuthGuard } from './auth/auth.guard';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 // Order matters when listing routes, make sure that routes are ordered from most specific
 // to least specific to prevent a general route from blocking more specific route checks
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    { path: 'recipes', component: RecipesComponent, canActivate: [AuthGuard], children: [
-        { path: '', component: RecipeStartComponent },
-        { path: 'new', component: RecipesEditComponent },
-        { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
-        { path: ':id/edit', component: RecipesEditComponent, resolve: [RecipesResolverService] }
-    ] },
-    { path: 'shopping-list', component: ShoppingListComponent },
-    { path: 'auth', component: AuthComponent }
+    { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) },
+    { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)},
+    { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)}
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes)],
+    imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })],
     exports: [RouterModule]
 })
 export class AppRoutingModule {
